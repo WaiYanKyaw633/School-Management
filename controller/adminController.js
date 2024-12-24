7f2b2dab5c756b5a7f48a8d4af0ff768f7e83db1
WaiYanKyaw633 <wyk657921@gmail.com>
2024-12-24 10:40:04 +0630

    aa

--- controller/adminController.js
+++ controller/adminController.js
@@ -1,26 +1,26 @@
 const sequelize = require('../config/db');
 const bcrypt = require('bcryptjs');
-const User = require('../models/user'); 
+const User = require('../models/user');
 const DataTypes = require('sequelize');
 const { Op } = require('sequelize');
 
 module.exports.createStudent = async (req, reply) => {
-    try { 
+    try {
         const { name, email, password } = req.body;
         const hashedPassword = await bcrypt.hash(password, 10);
         const student = await User.create({ name, email, password: hashedPassword, role: "student" });
-        reply.send({message:"Student Created Successfully",student});
+        reply.send({status:true, message: "Student Created Successfully", student });
     } catch (err) {
         reply.code(500).send({ status: false, error: err.message, message: "Failed to create student" });
     }
 };
 
 module.exports.createTeacher = async (req, reply) => {
-    try { 
+    try {
         const { name, email, password } = req.body;
         const hashedPassword = await bcrypt.hash(password, 10);
         const teacher = await User.create({ name, email, password: hashedPassword, role: "teacher" });
-        reply.send({message:"Teacher Created Successfully" ,teacher});
+        reply.send({status:true, message: "Teacher Created Successfully", teacher });
     } catch (err) {
         reply.code(500).send({ status: false, error: err.message, message: "Failed to create teacher" });
     }
@@ -28,35 +28,35 @@ module.exports.createTeacher = async (req, reply) => {
 
 module.exports.UpdateUser = async (req, reply) => {
     try {
-        const { id } = req.params;  
-        const { name, email, password, role } = req.body;  
+        const { id } = req.params;
+        const { name, email, password, role } = req.body;
         if (email) {
             const existingUser = await User.findOne({ where: { email, id: { [Op.ne]: id } } });
             if (existingUser) {
-                return reply.code(400).send({ message: "Email is already taken by another user." });
+                return reply.code(400).send({status:false,message: "Email is already taken by another user." });
             }
         }
-            const validRole= ['student' , 'teacher'];
-            if (role && !validRole.includes(role)){
-                return reply.code(400).send ({message: "invalid role: choose student or teacher"});
-            }
-        const hashedPassword = password ? await bcrypt.hash(password, 10): undefined;
+        const validRole = ['student', 'teacher'];
+        if (role && !validRole.includes(role)) {
+            return reply.code(400).send({status:false,message: "invalid role: choose student or teacher" });
+        }
+        const hashedPassword = password ? await bcrypt.hash(password, 10) : undefined;
         const updateData = {};
         if (name) updateData.name = name;
         if (email) updateData.email = email;
         if (password) updateData.password = hashedPassword;
         if (role) updateData.role = role;
-       
-            const updatedUser = await User.update(
+
+        const updatedUser = await User.update(
             updateData,
             { where: { id } }
         );
-         if (updatedUser === 0) {
-            return reply.code(404).send({ message: "User not found" });
+        if (updatedUser === 0) {
+            return reply.code(404).send({status:false,message: "User not found" });
         }
         const updatedUserData = await User.findByPk(id);
-        reply.send({ message: "User updated successfully", user: updatedUserData });
-        } catch (err) {
+        reply.send({status:true,message: "User updated successfully", user: updatedUserData });
+    } catch (err) {
         console.error("Error during user update:", err);
         reply.code(500).send({ status: false, error: err.message, message: "Failed to update user" });
     }
@@ -68,16 +68,16 @@ module.exports.deleteUser = async (req, reply) => {
         const user = await User.findByPk(id);
         if (!user) {
             console.log(`User with ID ${id} not found.`);
-            return reply.code(404).send({ message: "User not found" });
+            return reply.code(404).send({status:false,message: "User not found" });
         }
         console.log(`User found:`, user);
         await User.destroy({ where: { id } });
         console.log(`User with ID ${id} deleted successfully.`);
-        reply.send({ message: "User deleted successfully" });
+        reply.send({status:true, message: `User with Id ${id} deleted successfully` });
 
     } catch (err) {
         console.error("Error during user deletion:", err);
-         reply.code(500).send({
+        reply.code(500).send({
             status: false,
             error: err.message,
             message: "Failed to delete user"
